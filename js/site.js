1f4b81d501af56c2aa145dd8d9c2f9c6a6903b3b
ShellyWonder <91100506+ShellyWonder@users.noreply.github.com>
2022-08-27 16:32:22 -0500

    add km js

--- js/site.js
+++ js/site.js
@@ -1,9 +1,9 @@
 //driver function used for display and passing values.
 function convertIt() {
 
-    //get the user entered value
+    //get the user-entered value
     let kmValue = parseFloat(document.getElementById("InputValue").value);
-
+  //Validation:
     if (isNaN(kmValue) ) {
         alert("Please enter a number");
 
@@ -11,13 +11,15 @@ function convertIt() {
         //call your function to conver the vm value to miles
         let milesValue = convertToKm(kmValue);
 
-        //display your name 
+        //display your results fixed to 2 dec places
         document.getElementById("results").innerHTML = `${kmValue} km = ${milesValue.toFixed(2)} miles`;
     }
 
 }
 
-//takes an array of strings and returns the longest one. 
+//performs equation Km * 0.6214 = Miles 
 function convertToKm(kmValue) {
-   return 0;
+  let milesValue = 0;
+  milesValue = kmValue * 0.6214;
+   return milesValue;
 }
