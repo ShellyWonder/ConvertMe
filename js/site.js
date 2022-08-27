3afb8fe86b654cdebb76a902076fe27a6d80eaa9
ShellyWonder <91100506+ShellyWonder@users.noreply.github.com>
2022-08-27 13:20:34 -0500

    scaffolding js

--- js/site.js
+++ js/site.js
@@ -1,54 +1,23 @@
-//CONTROLLER 
-function getValues() {
+//driver function used for display and passing values.
+function convertIt() {
 
-}
-//PROGRAM ENTRY POINTS
-function getValues2() {
-  let startValue = document.getElementById("startValue").value;
-  let endValue = document.getElementById("endValue").value;
+    //get the user entered value
+    let kmValue = parseFloat(document.getElementById("InputValue").value);
 
-  //VALIDATION
-  startValue = parseInt(startValue);
-  endValue = parseInt(endValue);
-
-  //check to see if input is integer
-  if (Number.isInteger(startValue) && Number.isInteger(endValue)) {
-    //generate list of numbers
-    let numbers = generateNumbers(startValue, endValue);
-    displayNumbers(numbers);
-  } else {
-    Swal.fire({
-      icon: "error",
-      title: "oops",
-      text: "only integers are allowed for JS Speedway"
-    })
-  }
-}
-
-function generateNumbers(start, end) {
-  let numbers = [];
-  for (let index = start; index <= end; index++) {
-    numbers.push(index);
-  }
-  return numbers;
-}
-
-function displayNumbers(numbers) {
-
-  let templateRows = "";
-  let className = "even";
-  for (let index = 0; index < numbers.length; index++) {
-
-    let number = numbers[index];
-
-    if (number % 2 === 0) {
-      className = "even";
+    if (isNaN(kmValue) ) {
+        alert("Please enter a number");
 
     } else {
-      className = "odd";
+        //call your function to conver the vm value to miles
+        let milesValue = convertToKm(kmValue);
+
+        //display your name 
+        document.getElementById("results").innerHTML = `${kmValue} km = ${milesValue.toFixed(2)} miles`;
     }
-    let row = `<tr><td class="${className}">${number}</td></tr>`;
-    templateRows += row;
-  }
-  document.getElementById("results").innerHTML = templateRows;
+
+}
+
+//takes an array of strings and returns the longest one. 
+function convertToKm(kmValue) {
+   return 0;
 }
